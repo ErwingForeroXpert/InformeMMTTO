4a478d279f5b7a0f6d3f76b3faa982be82d2c646
ErwingForeroXpert <erwing.forero@xpertgroup.co>
2021-12-21 10:20:18 -0500

    Regular auto-commit at 10:20:18 on 21/12/2021

--- utils.py
+++ utils.py
@@ -52,20 +52,22 @@ def getMostRecentFile(path, _filter=None):
 
 def getIntervalDates(_dates):
     conv_dates = []
-    conv_dates.append([int(float(sub_num)) for sub_num in _dates[0] if ''.join(char for char in str(sub_num) if char.isdigit()) != ''])
+    for temp_date in _dates:
+        conv_dates.append([int(float(sub_num)) for sub_num in temp_date if ''.join(char for char in str(sub_num) if char.isdigit()) != ''])
+    conv_dates.append()
     conv_dates.append([int(float(sub_num)) for sub_num in _dates[1] if ''.join(char for char in str(sub_num) if char.isdigit()) != ''])
     
-    temp_dates = []
-    if len(conv_dates[0]) > 1:
-        years_sort = sorted(conv_dates[0]) #2011, 2012,...
-        moths_sort = sorted(conv_dates[1], reverse=True) #12, 11, 10
-        for month in moths_sort: 
-            if abs(moths_sort[0] - month) < 4:
-                temp_dates.append([int(years_sort[0]), int(month)])
-            else:
-                temp_dates.append([int(years_sort[1]), int(month)])
-    else:
-        temp_dates = [[int(conv_dates[0][0]), int(month)] for month in conv_dates[1]]
+    # # temp_dates = []
+    # # if len(conv_dates[0]) > 1:
+    # #     years_sort = sorted(conv_dates[0]) #2011, 2012,...
+    # #     moths_sort = sorted(conv_dates[1], reverse=True) #12, 11, 10
+    # #     for month in moths_sort: 
+    # #         if abs(moths_sort[0] - month) < 4:
+    # #             temp_dates.append([int(years_sort[0]), int(month)])
+    # #         else:
+    # #             temp_dates.append([int(years_sort[1]), int(month)])
+    # # else:
+    # #     temp_dates = [[int(conv_dates[0][0]), int(month)] for month in conv_dates[1]]
 
     return temp_dates
 
